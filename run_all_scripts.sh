ff71159b4a76dfca40bc5bdae198a8cfb1715288
yasuke.kuroda <yasuke.kuroda@diezon.co.jp>
2026-01-19 11:41:11 +0900

    a

--- run_all_scripts.sh
+++ run_all_scripts.sh
@@ -10,14 +10,6 @@
 ./script__sql/local_application_cat.sh # 空出力が期待値.
 ./script__sql/local_application_import.sh
 
-# ※以下のコメントアウトは外さないこと.
-# ※以下のコメントアウトを外して本スクリプト実行したら、ローカルDB壊れます.
-
-# api gatewayはSTGからローカルにコピー不要. 
-# # gateway
-# ./script__sql/staging_gateway_export.sh
-# ./script__sql/copy_gateway_stg_to_local.sh
-# ./script__sql/rename_gateway_db_user_stg_to_local.sh
-# ./script__sql/local_gateway_delete.sh
-# ./script__sql/local_gateway_cat.sh # 空出力が期待値.
-# ./script__sql/local_gateway_import.sh
+## DB migrate.
+docker exec -i regolith-admin-api ash -c "npx -y prisma migrate dev"
+# docker exec -i olta-admin-api ash -c "npx -y prisma migrate dev"
